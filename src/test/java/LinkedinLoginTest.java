5d903d3a5ea3bc2e0c27cbd1f63053aec0c46ea2
Maksym <mbenivskiy@ukr.net>
2018-08-06 20:07:09 +0300

    Hometask 3

--- src/test/java/LinkedinLoginTest.java
+++ src/test/java/LinkedinLoginTest.java
@@ -9,6 +9,27 @@ public class LinkedinLoginTest {
 
     @Test                                       //аннотация для компилятора о том, что метод можно выполнить
     public void successfulLoginTest() {
+        WebDriver browser = new FirefoxDriver();
+        browser.get("https://www.linkedin.com/");
+        WebElement userEmailField = browser
+                .findElement(By.xpath("//input[@id='login-email']"));
+        WebElement userPasswordField = browser
+                .findElement(By.xpath("//input[@id='login-password']"));
+        WebElement signInButton = browser
+                .findElement(By.xpath("//input[@id='login-submit']"));
+
+        userEmailField.sendKeys("youngbloodvasilievna@gmail.com");
+        userPasswordField.sendKeys("Pensiya15000");
+        signInButton.click();
+
+        String pageURL = browser.getCurrentUrl();
+        Assert.assertEquals(pageURL, "https://www.linkedin.com/feed/", "Page URL is wrong");
+
+        String pageTitle = browser.getTitle();
+        Assert.assertEquals(pageTitle,"LinkedIn", "Page Title is wrong");
+
+        WebElement welcomeMessage = browser.findElement(By.xpath("//*[@data-control-name='identity_welcome_message']"));
+        Assert.assertEquals(welcomeMessage.getText(), "Добро пожаловать, Elena!", "Page doesn't contain Welcome Message");
     }
 
     @Test
